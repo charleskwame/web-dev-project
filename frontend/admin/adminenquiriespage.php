34bb3929a9871fbdddd789814a71a077baf288d1
T-Charles <charlestettehnull@gmail.com>
2025-07-16 20:09:53 +0000

    Fixed the changes not showing up on reload by adding a .htaccess file with a no cache option included

--- frontend/admin/adminenquiriespage.php
+++ frontend/admin/adminenquiriespage.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./adminenquiriespage.css">
+    <link rel="stylesheet" href="./adminenquiriespage.css?v= <?php echo date('his'); ?>">
     <title>Admin Enquiries Dashboard</title>
 </head>
 
